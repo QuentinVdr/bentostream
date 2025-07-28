455b8964ce374e38ce76b11ac05fcb0f28261150
Quentin VERDIER <quent1.verdier@gmail.com>
2025-07-28 22:52:59 +0200

    ✨ add hover effect for title in StreamGridItem component

--- src/components/StreamGridItem/StreamGridItem.tsx
+++ src/components/StreamGridItem/StreamGridItem.tsx
@@ -1,4 +1,4 @@
-import { memo, useMemo } from 'react';
+import { memo, useMemo, useState } from 'react';
 
 interface StreamGridItemProps {
   streamName: string;
@@ -7,6 +7,7 @@ interface StreamGridItemProps {
 }
 
 const StreamGridItem = memo(({ streamName, isChat = false, isDarkThemePreferred = false }: StreamGridItemProps) => {
+  const [isTitleHovered, setIsTitleHovered] = useState(false);
   const title = useMemo(() => (isChat ? `Chat: ${streamName}` : `Stream: ${streamName}`), [isChat, streamName]);
 
   const iframeSrc = useMemo(() => {
@@ -18,11 +19,27 @@ const StreamGridItem = memo(({ streamName, isChat = false, isDarkThemePreferred 
   }, [streamName, isChat, isDarkThemePreferred]);
 
   return (
-    <div className="flex h-full w-full flex-col flex-nowrap overflow-hidden rounded border border-gray-300/20 bg-zinc-900">
-      <div>
-        <h2 className="text-center text-base font-semibold">{title}</h2>
+    <div
+      className="group flex h-full w-full flex-col flex-nowrap overflow-hidden rounded border border-gray-300/20 bg-zinc-900"
+      onMouseLeave={() => setIsTitleHovered(false)}
+    >
+      <div
+        className="title-overlay absolute z-10 w-full origin-top scale-y-0 transform-gpu transition-transform duration-300 ease-in-out group-hover:scale-y-100 hover:z-20"
+        onMouseEnter={() => setIsTitleHovered(true)}
+      >
+        <h2 className="bg-zinc-900 px-4 py-2 text-center text-base font-semibold text-white backdrop-blur-sm">
+          {title}
+        </h2>
       </div>
-      <iframe title={title} src={iframeSrc} height="100%" width="100%" allowFullScreen={!isChat} loading="lazy" />
+      <iframe
+        title={title}
+        src={iframeSrc}
+        height="100%"
+        width="100%"
+        allowFullScreen={!isChat}
+        loading="lazy"
+        className={`transition-all duration-300 ease-in-out ${isTitleHovered ? 'mt-8' : ''}`}
+      />
     </div>
   );
 });
