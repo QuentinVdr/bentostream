9fc076c3d926b8128f38c4f5a5a8f52ec3a7b5e3
Quentin VERDIER <quent1.verdier@gmail.com>
2025-07-29 05:12:54 +0200

    ✨ Update styles for body in index.css and simplify layout in __root.tsx

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -4,9 +4,7 @@ import { TanStackRouterDevtools } from '@tanstack/react-router-devtools';
 export const Route = createRootRoute({
   component: () => (
     <>
-      <div className="min-h-dvh w-dvw bg-zinc-950 text-white">
-        <Outlet />
-      </div>
+      <Outlet />
       <TanStackRouterDevtools />
     </>
   ),
