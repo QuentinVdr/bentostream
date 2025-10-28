7d57b831aa395b1f8577a63894357d7be3038492
Quentin  VERDIER <quent1.verdier@gmail.com>
2025-10-28 16:55:05 +0100

    🔧 remove deprecated forwardRef and simplify props handling

--- src/components/gridItems/GridItem/GridItem.tsx
+++ src/components/gridItems/GridItem/GridItem.tsx
@@ -1,5 +1,5 @@
 import { useIframeRefresh } from '@/hooks/useIframeRefresh';
-import { forwardRef, memo, useImperativeHandle, type ReactNode } from 'react';
+import { memo, useImperativeHandle, type ReactNode, type Ref } from 'react';
 import GridItemHeader from './GridItemHeader/GridItemHeader';
 
 interface GridItemProps {
@@ -9,44 +9,41 @@ interface GridItemProps {
   streamName?: string;
   onRefresh?: () => void;
   children?: ReactNode;
+  ref?: Ref<GridItemRef>;
 }
 
 export interface GridItemRef {
   refreshIframe: () => void;
 }
 
-const GridItem = memo(
-  forwardRef<GridItemRef, GridItemProps>(
-    ({ title, iframeSrc, allowFullScreen, streamName, onRefresh, children }, ref) => {
-      const { iframeRef, refreshIframe } = useIframeRefresh(onRefresh);
+const GridItem = memo(({ title, iframeSrc, allowFullScreen, streamName, onRefresh, children, ref }: GridItemProps) => {
+  const { iframeRef, refreshIframe } = useIframeRefresh(onRefresh);
 
-      useImperativeHandle(ref, () => ({ refreshIframe }), [refreshIframe]);
+  useImperativeHandle(ref, () => ({ refreshIframe }), [refreshIframe]);
 
-      return (
-        <section
-          className="group flex h-full w-full flex-col flex-nowrap overflow-hidden rounded border border-gray-300/20 bg-zinc-900"
-          aria-label={title}
-        >
-          <GridItemHeader title={title} streamName={streamName}>
-            {children}
-          </GridItemHeader>
-          <iframe
-            ref={iframeRef}
-            key={iframeSrc}
-            title={title}
-            src={iframeSrc}
-            height="100%"
-            width="100%"
-            allowFullScreen={allowFullScreen}
-            loading="lazy"
-            className="transition-all duration-300 ease-in-out group-hover:mt-12"
-            style={{ transform: 'translateZ(0)' }}
-          />
-        </section>
-      );
-    }
-  )
-);
+  return (
+    <section
+      className="group flex h-full w-full flex-col flex-nowrap overflow-hidden rounded border border-gray-300/20 bg-zinc-900"
+      aria-label={title}
+    >
+      <GridItemHeader title={title} streamName={streamName}>
+        {children}
+      </GridItemHeader>
+      <iframe
+        ref={iframeRef}
+        key={iframeSrc}
+        title={title}
+        src={iframeSrc}
+        height="100%"
+        width="100%"
+        allowFullScreen={allowFullScreen}
+        loading="lazy"
+        className="transition-all duration-300 ease-in-out group-hover:mt-12"
+        style={{ transform: 'translateZ(0)' }}
+      />
+    </section>
+  );
+});
 
 GridItem.displayName = 'GridItem';
 
